b20d46160f40a8f2d0fe67102be6299ab33a50a7
Albert <bubokastor@mail.ru>
2015-08-24 17:46:53 +0300

    Add babylon

---
+++ FireworksServer/ClientHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace FireworksServer
+{
+    public class ClientHub: Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+        public void SendCoord(int x, int y)
+        {
+            Clients.All.broadcastMessage(x, y);
+        }
+    }
+}
---
+++ FireworksServer/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using FireworksServer;
+[assembly: OwinStartup(typeof(FireworksServer.Startup1))]
+
+namespace FireworksServer
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Дополнительные сведения о настройке приложения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
