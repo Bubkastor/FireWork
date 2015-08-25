4e309c1d9b42e56b398907eef40a8aa442342962
Albert <bubokastor@mail.ru>
2015-08-25 16:54:19 +0300

    add file partical

---
+++ FireworksServer/MyScript/App.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+document.addEventListener('DOMContentLoaded', start, false);
+
+function start(){
+    console.log('start');
+}
