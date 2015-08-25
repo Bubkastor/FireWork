ebbfd2927c39c3851dac0a08111ef364f3d5b360
Albert <bubokastor@mail.ru>
2015-08-25 18:19:22 +0300

    Refactoring

--- FireworksServer/MyScript/App.js
+++ FireworksServer/MyScript/App.js
@@ -12,5 +12,59 @@ window.requestAnimFrame = (function(){
 document.addEventListener('DOMContentLoaded', start, false);
 
 function start(){
-    console.log('start');
+    var chat = $.connection.clientHub;
+
+    var canvas = document.getElementById('renderCanvas');
+
+
+    var engine = new BABYLON.Engine(canvas, true);
+
+    // createScene function that creates and return the scene
+    var createScene = function () {
+        var scene = new BABYLON.Scene(engine);
+
+        var camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(0, 0, 10), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        camera.attachControl(canvas, false);
+        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+        var wall = BABYLON.Mesh.CreatePlane("wall", 40.0, scene);
+        wall.material = new BABYLON.StandardMaterial("wallMat", scene);
+        wall.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+
+
+        scene.onPointerDown = function (evt, pickResult) {
+            if (pickResult.hit) {
+                var x = pickResult.pickedPoint.x;
+                var y = pickResult.pickedPoint.y;
+                var position = new BABYLON.Vector3(x, y, 0);
+                $.connection.hub.start().done(function(){
+                    chat.server.send(x, y);
+                });
+            }
+        };
+
+        return scene;
+    };
+
+    var scene = createScene();
+    chat.client.broadcastMessage = function (x, y) {
+        var position = new BABYLON.Vector3(x, y, 0);
+        //CreateBoom(scene, position);
+        var boom = new ParticalSystem({'position': position, /*'music': new BABYLON.Sound("fireworks", "sounds/fireworks.mp3", scene,
+            function(){
+                console.log('Sound loaded')
+            },{ autoplay: true})*/
+        }, scene);
+        boom.start();
+    };
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+
+    window.addEventListener('resize', function () {
+        engine.resize();
+    });
+
 }
--- FireworksServer/MyScript/Partical.js
+++ FireworksServer/MyScript/Partical.js
@@ -1,3 +1,65 @@
 ﻿'use strict';
 
-function
+var TIME_LIFE = 3000;
+var TIME_REMOVE = 500;
+class ParticalSystem{
+    constructor(option, scene){
+        this.option = option;
+        this.fountain = BABYLON.Mesh.CreateBox("point", 0.1, scene);
+        this.fountain.visibility = 0.0;
+        this.fountain.position = option.position || new BABYLON.Vector3(0, 0, 0);
+
+        option.music && (this.music = option.music) ;
+        option.music && this.music.attachToMesh(this.fountain);
+
+        this.particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+        this.particleSystem.particleTexture = option.texture || new BABYLON.Texture("textures/flare.png", scene);
+
+        this.particleSystem.emitter = this.fountain;
+
+        this.particleSystem.minEmitBox = new BABYLON.Vector3(0, 0, 0);
+        this.particleSystem.maxEmitBox =  new BABYLON.Vector3(0, 0, 0);
+
+        this.particleSystem.color1 = option.color1 || new BABYLON.Color4(1.0, 0.0, 0.0, 1.0);
+        this.particleSystem.color2 = option.color2 || new BABYLON.Color4(1.0, 0.5, 0.0, 1.0);
+
+        this.particleSystem.colorDead = new BABYLON.Color4(0.0, 0.0, 0.0, 0.5);
+
+        this.particleSystem.minSize = 0.09;
+        this.particleSystem.maxSize = 0.2;
+
+        this.particleSystem.minLifeTime = option.minLifeTime || 0.3;
+        this.particleSystem.maxLifeTime = option.maxLifeTime || 0.7;
+        this.option.maxLifeTime =  this.particleSystem.maxLifeTime;
+
+        this.particleSystem.emitRate = option.emitRate || 1000;
+
+        this.particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+
+        this.particleSystem.gravity = new BABYLON.Vector3(0, -11, 0);
+
+        this.particleSystem.direction1 = option.direction1 || new BABYLON.Vector3(1, 1, 0);
+        this.particleSystem.direction2 = option.direction2 || new BABYLON.Vector3(-1, 1, 2);
+
+        this.particleSystem.minAngularSpeed = 0;
+        this.particleSystem.maxAngularSpeed = Math.PI;
+
+        this.particleSystem.minEmitPower = 1;
+        this.particleSystem.maxEmitPower = 5;
+        this.particleSystem.updateSpeed = 0.005;
+        option = null;
+    }
+
+    start(){
+        this.particleSystem.start();
+        var self = this;
+        setTimeout(function(){
+            self.particleSystem.stop();
+            setTimeout(function(){
+                self.fountain.dispose();
+                self.option.music && self.music.dispose();
+            },TIME_LIFE + self.option.maxLifeTime);
+
+        },TIME_REMOVE + self.option.maxLifeTime);
+    }
+}
