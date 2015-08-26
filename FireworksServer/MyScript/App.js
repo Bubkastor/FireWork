896d3ea595728781108a6dc0241948371d4a8d12
Albert <bubokastor@mail.ru>
2015-08-26 15:12:58 +0300

    refactoring scena

--- FireworksServer/MyScript/App.js
+++ FireworksServer/MyScript/App.js
@@ -16,43 +16,23 @@ function start(){
 
     UserCount(connectionServer);
     var canvas = document.getElementById('renderCanvas');
-
-
     var engine = new BABYLON.Engine(canvas, true);
 
-    var createScene = function () {
-        var scene = new BABYLON.Scene(engine);
+    var scene = Scena(engine, canvas);
 
-        var camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(0, 0, 10), scene);
-
-        camera.setTarget(new BABYLON.Vector3(0,0,0));
-
-        camera.attachControl(canvas, false);
-        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-
-        var wall = BABYLON.Mesh.CreatePlane("wall", 40.0, scene);
-        wall.material = new BABYLON.StandardMaterial("wallMat", scene);
-        wall.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
-
-
-        scene.onPointerDown = function (evt, pickResult) {
-            if (pickResult.hit) {
-                var x = pickResult.pickedPoint.x;
-                var y = pickResult.pickedPoint.y;
-				appInsights.trackEvent("click");
-                $.connection.hub.start().done(function(){
-                    connectionServer.server.send(x, y);
-                });
-            }
-        };
-
-        return scene;
+    scene.onPointerDown = function (evt, pickResult) {
+        if (pickResult.hit) {
+            var x = pickResult.pickedPoint.x;
+            var y = pickResult.pickedPoint.y;
+            appInsights.trackEvent("click");
+            $.connection.hub.start().done(function () {
+                connectionServer.server.send(x, y);
+            });
+        }
     };
-
-    var scene = createScene();
     connectionServer.client.broadcastMessage = function (x, y) {
         var position = new BABYLON.Vector3(x, y, 0);
-        var boom = new ParticalSystem({'position': position, /*'music': new BABYLON.Sound("fireworks", "sounds/fireworks.mp3", scene,
+        var boom = new ParticalSystem({'position': position/*, 'music': new BABYLON.Sound("fireworks", "sounds/fireworks.mp3", scene,
             function(){
                 console.log('Sound loaded')
             },{ autoplay: true})*/
---
+++ FireworksServer/MyScript/Scena.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+function Scena(engine, canvas) {
+    var scene = new BABYLON.Scene(engine, canvas);
+
+    var camera = new BABYLON.TargetCamera('camera1', new BABYLON.Vector3(0, 0, 10), scene);
+
+    camera.setTarget(new BABYLON.Vector3(0, 0, 0));
+
+    camera.attachControl(canvas, false);
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+    var wall = BABYLON.Mesh.CreatePlane("wall", 40.0, scene);
+    wall.material = new BABYLON.StandardMaterial("wallMat", scene);
+    wall.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+
+
+
+
+    return scene;
+}
